8a854e113d212ca15fbda7c3cfdaf91571dc46bc
anarusu <rusu.annaa@gmail.com>
2025-06-21 23:03:06 +0300

    Add ProfessorService and ProfessorExceptions

---
+++ Exceptions/ProfessorExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApp.Exceptions
+{
+    public class ProfessorExceptions : Exception
+    {
+        public ProfessorExceptions() : base("An error occurred with the professor operation.") { }
+        public ProfessorExceptions(string message) : base(message) { }
+        public ProfessorExceptions(string message, Exception innerException) : base(message, innerException) { }
+        public static ProfessorExceptions ProfessorNotFound(Guid professorId)
+        {
+            return new ProfessorExceptions($"Professor with ID {professorId} not found.");
+        }
+        public static ProfessorExceptions InvalidProfessorData(string message)
+        {
+            return new ProfessorExceptions(message);
+        }
+    }
+}
---
+++ Services/ProfessorService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolApp.Exceptions;
+using SchoolApp.Models;
+using SchoolApp.Repositories;
+
+namespace SchoolApp.Services
+{
+    public class ProfessorService(InMemoryRepository<Professor> professorRepository)
+    {
+        private readonly InMemoryRepository<Professor> _professorRepository = professorRepository;
+        public void AddProfessor(Professor professor)
+        {
+            ArgumentNullException.ThrowIfNull(professor);
+            _professorRepository.Add(professor);
+        }
+
+        public void RemoveProfessor(Guid professorId)
+        {
+            _professorRepository.Remove(professorId);
+        }
+
+        public Professor GetProfessorById(Guid professorId)
+        {
+            var professor = _professorRepository.GetById(professorId);
+            return professor ?? throw ProfessorExceptions.ProfessorNotFound(professorId);
+        }
+
+
+        public IEnumerable<Professor> GetAllProfessors()
+        {
+            return _professorRepository.GetAll();
+        }
+
+        public void UpdateProfessor(Guid professorId, Professor updateProfessor)
+        {
+            ArgumentNullException.ThrowIfNull(updateProfessor);
+            if (professorId != updateProfessor.ProfessorId) throw ProfessorExceptions.InvalidProfessorData("Professor ID mismatch.");
+            _ = _professorRepository.GetById(professorId) ?? throw ProfessorExceptions.ProfessorNotFound(professorId);
+            _professorRepository.Update(professorId, updateProfessor);
+        }
+    }
+}
